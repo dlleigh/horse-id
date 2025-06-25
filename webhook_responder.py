6509de301d7501e1b5bfab4f84b858c68fe2535b
David Leigh <username@users.noreply.github.com>
2025-06-24 21:26:24 -0400

    improve response message formatting

--- webhook_responder.py
+++ webhook_responder.py
@@ -41,7 +41,7 @@ def webhook_handler(event, context):
         }
 
     response_message = (
-        "Identification started! (Please note: By sending a picture to the Little Tree Farms Horse ID number, "
+        "Identification started! \n\n(Please note: By sending a picture to the Little Tree Farms Horse ID number, "
         "you consent to receive a response. Respond STOP to stop receiving messages.)"
     )
     twilio_response_body = f'<Response><Message>{response_message}</Message></Response>'
